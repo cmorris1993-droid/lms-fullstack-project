4d9093c09fb874a613ef54771657273848327ef2
Cyan Morris <cmorris1993@googlemail.com>
2025-12-31 14:29:31 +0000

    Add STATIC_ROOT for production

--- backend/settings.py
+++ backend/settings.py
@@ -75,6 +75,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CORS_ALLOW_ALL_ORIGINS = True
 
